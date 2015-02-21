eaef8b295083c50fa843e817f74e2e7317d81abb
Ryo Suzuki <ryooopan@googlemail.com>
2015-02-21 11:59:29 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-if [ ! -d "$HOME/.files" ]; then
+if [ ! -d ~/.files ]; then
   echo "Installation starts..."
-  git clone https://github.com/ryooopan/dotfiles.git "$HOME/.files"
-  cd "$HOME/.files"
-  ln -sf .vimrc "$HOME/.vimrc"
-  ln -sf .zshrc "$HOME/.zshrc"
-  ln -sf .gitconfig "$HOME/.gitconfig"
+  git clone https://github.com/ryooopan/dotfiles.git ~/.files
+  cd ~/.files
+  ln -sf .vimrc ~/.vimrc
+  ln -sf .zshrc ~/.zshrc 
+  ln -sf .gitconfig ~/.gitconfig
+  cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+  ln -sf ~/.files/sublime User
 else
   echo ".files is already installed"
 fi
