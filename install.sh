2b84126c39422ec518ac33edae0b18c8f6c1e569
Ryo Suzuki <ryosuzk@gmail.com>
2016-10-29 14:34:09 -0600

    Update

--- install.sh
+++ install.sh
@@ -4,9 +4,8 @@ if [ ! -d ~/.files ]; then
   echo "Installation starts..."
   git clone git@github.com:ryosuzuki/dotfiles.git ~/.files
   ln -sf ~/.files/.vimrc ~/.vimrc
-  ln -sf ~/.files/.zshrc ~/.zshrc 
+  ln -sf ~/.files/.zshrc ~/.zshrc
   ln -sf ~/.files/.gitconfig ~/.gitconfig
-  ln -sf ~/.files/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/  
   ln -sf ~/.files/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
 else
   echo ".files is already installed"
